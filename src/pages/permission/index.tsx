3511279c3fdeb498c87ed05843cdfad141ae694c
biztechadvisors <advisorsbiztech@gmail.com>
2024-01-25 13:35:44 +0530

    Dealer

--- src/pages/permission/index.tsx
+++ src/pages/permission/index.tsx
@@ -177,19 +177,21 @@ export default function Permission() {
                         ))
                       : ''}
                   </td>
-                  <td className="border p-2">
-                  <td className="border p-2">
                   {canWrite ? (
+                  <td className="border p-2">
+                  <td className="border p-2">
+                  
   <Link href={`permission/create/`}>
     <button className="text-blue-500 hover:underline flex items-center space-x-1">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"></path></svg>
       <span>View</span>
     </button>
   </Link>
-  ) : null}
+ 
 </td>
 
 </td>
+ ) : null}
                 </tr>
               ))}
             </tbody>
