d4d7337d8b756c11484d6e6f16f3ba644ae6faa0
biztechadvisors <advisorsbiztech@gmail.com>
2024-01-25 12:05:12 +0530

    change shop

--- src/components/dealerlist/add-dealer-form.tsx
+++ src/components/dealerlist/add-dealer-form.tsx
@@ -292,7 +292,6 @@ export default function CreateOrUpdateDealerForm({ initialValues, id }: IProps) 
   const { mutate: createDealer, isLoading: creating } = useAddDealerMutation();
   const { mutate: updateDealer, isLoading: updating } = useUpdateDealerMutation();
   const onSubmit = (values: FormValues) => {
-    console.log("moye moye value", values)
     const isActiveVal: any = values.isActive
     const input = {
       language: router.locale!,
@@ -307,8 +306,6 @@ export default function CreateOrUpdateDealerForm({ initialValues, id }: IProps) 
       dealerCategoryMargins: values.dealerCategoryMargins,
       dealerProductMargins: values.dealerProductMargins,
     };
-
-    // console.log("moye moye value", input)
 
 
     if (!initialValues) {
