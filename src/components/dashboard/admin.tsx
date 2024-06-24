902b989dff8352f31198de379315f1d63ef4f674
Ajay Malviya <biztechadvisors@gmail.com>
2024-06-24 18:12:52 +0530

    first commit

--- src/components/dashboard/admin.tsx
+++ src/components/dashboard/admin.tsx
@@ -53,7 +53,7 @@ export default function Dashboard() {
   } = usePopularProductsQuery({
     limit: 10,
     language: locale,
-    shop_id: meData?.shop_id ? meData.shops : meData?.managed_shop?.id,
+    shop_id: meData?.managed_shop?.id,
   });
 
   if (analyticsLoading || orderLoading || popularProductLoading) {
