198700ec47fe65045c1c55880e875a29aba122e6
biztechadvisors <advisorsbiztech@gmail.com>
2025-04-05 12:29:02 +0530

    commit chagnes

--- src/components/product/product-variable-form.tsx
+++ src/components/product/product-variable-form.tsx
@@ -43,6 +43,8 @@ export default function ProductVariableForm({
     language: locale,
   });
 
+  // console.log("attributes 45 --- ", attributes)
+
   const { register, control, watch, setValue, getValues, formState: { errors } } = useFormContext();
 
   const variations = watch('variations');
@@ -85,6 +87,7 @@ export default function ProductVariableForm({
   }, [initialValues]);
 
   const handleVariationChange = (fieldIndex: number, attributeIndex: number, selected: any, isAttribute: boolean) => {
+
     if (isAttribute) {
       setValue(`variation_options[${fieldIndex}].attributes[${attributeIndex}].attribute`, selected);
     } else {
@@ -94,7 +97,6 @@ export default function ProductVariableForm({
     const attributes = watch(`variation_options[${fieldIndex}].attributes`);
 
     // const allVariations = watch('variations') || [];
-    console.log("attributes ", attributes)
 
     const attributeValues = attributes.reduce((acc: any[], attr: any) => {
       // Check if attr.value is an array before calling .map
@@ -109,7 +111,7 @@ export default function ProductVariableForm({
     }, []);
 
     setValue('variations', attributeValues);
-    console.log("variations ", variations)
+    // console.log("variations ", variations)
 
     const options = attributes.map((attr: any) => ({
       name: attr.attribute.name,
@@ -172,14 +174,25 @@ export default function ProductVariableForm({
                     <div className="mt-5 rounded border border-gray-300 p-5">
                       <div className="grid gap-5">
                         {attributes?.items?.map((attribute, attributeIndex) => {
+                          // console.log("attribute 177 --- ", attribute)
+                          // console.log("attributeIndex 178 --- ", attributeIndex)
+
+                          // console.log("initialValues 181 --- ", initialValues.variation_options)
 
                           const initialOption = initialValues?.variation_options?.[fieldIndex];
+                          if (initialOption) {
+
+                          }
+                          // console.log("initialOption --- 187 ", initialOption)
 
                           const initialAttribute = initialOption?.options?.[attributeIndex];
+                          console.log("initialAttribute --- 190 ", initialAttribute)
 
                           const initialAttributeValue = initialAttribute?.value || [];
+                          // console.log("initialAttributeValue --- 193 ", initialAttributeValue)
 
                           const initialAttributeName = initialAttribute?.attribute || null;
+                          console.log("initialAttributeName ---- 196 ", initialAttributeName)
 
                           return (
                             <div key={attribute.id} className="flex flex-wrap items-center">
